8298bb9b0727df1786fcdc9855356207839bebdb
Willslima <williansantos38@gmail.com>
2022-07-20 16:51:52 -0300

    Aplicativo Alcool ou Gasolina

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public resultado: String = "Resultado"
+  public precoGasolina: any;
+  public precoAlcool: any;
+
+  calcular() {
+    if(this.precoAlcool && this.precoGasolina){
+      // this.resultado = "Preenchidos"
+
+      let pAlcool = parseFloat(this.precoAlcool)
+      let pGasolina = parseFloat(this.precoGasolina)
+      let res = pAlcool / pGasolina // >= 0.7 ? "É melhor utilizar Gasolina" : "É melhor utilizar Álcool"
+      if(res >= 0.7){
+        this.resultado = "É melhor utilizar Gasolina" 
+      } else {
+        this.resultado = "É melhor utilizar Álcool"
+      }
+
+    }else{
+      this.resultado = "Álcool ou Gasolina não foram preenchidos"
+    }
+  }
 
 }
